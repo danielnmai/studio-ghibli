7c126cc9796e4c5455617c137c3c70b947878586
Daniel Mai <danielmai1728@gmail.com>
2018-02-01 22:35:39 -0800

    complete render favorite characters

--- src/FavoritePage/Favorite.js
+++ src/FavoritePage/Favorite.js
@@ -5,10 +5,106 @@ class Favorite extends React.Component {
   constructor(props){
     super(props)
   }
-  render(){
-    console.log(this.props.favorites)
+  favoriteCharacters = () => {
+    const renderCharacter = (info) =>{
+      return (
+        <tr key={info.id} onClick={this.onClick}>
+          <td>
+            <div className='row'>
+              <div className='col-sm-4'>
+                <strong>Name</strong>
+              </div>
+              <div className='col-sm-4'>{ info.name }</div>
+            </div>
+            <div className='row'>
+              <div className='col-sm-4'>
+                <strong>Gender</strong>
+              </div>
+              <div className='col-sm-4'>{ info.gender }</div>
+            </div>
+            <div className='row'>
+              <div className='col-sm-4'>
+                <strong>Age</strong>
+              </div>
+              <div className='col-sm-4'>{ info.age }</div>
+            </div>
+            <div className='row'>
+              <div className='col-sm-4'>
+                <strong>Eye Color</strong>
+              </div>
+              <div className='col-sm-4'>{ info.eye_color }</div>
+            </div>
+            <div className='row'>
+              <div className='col-sm-4'>
+                <strong>Hair Color</strong>
+              </div>
+              <div className='col-sm-4'>{ info.hair_color }</div>
+            </div>
+            {/* <div>
+              {
+                showDetailInfo ? (
+                  this.renderDetailInfo()) : null
+              }
+            </div> */}
+            </td>
+        </tr>
+
+      )
+    }
     return (
-      <div> This is your list of favorites</div>
+      <table className="table table-hover">
+      <thead>
+        <tr>
+          <th>
+            <div className='row'>
+              <div className='col-sm-5'>
+                <strong>Your Favorite Characters</strong>
+              </div>
+            </div>
+
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {this.props.favorites.map(data => renderCharacter(data))}
+      </tbody>
+    </table>
+    )
+  }
+  favoriteFilms = () => {
+    return (
+      <table className="table table-hover">
+      <thead>
+        <tr>
+          <th>
+            <div className='row'>
+              <div className='col-sm-5'>
+                <strong>Your Favorite Films</strong>
+              </div>
+            </div>
+
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* {chars.map(data => this.renderCharacterInfo(data))} */}
+      </tbody>
+    </table>
+    )
+  }
+  render(){
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>
+            {this.favoriteCharacters()}
+          </div>
+          <div className='col'>
+            {this.favoriteFilms()}
+          </div>
+        </div>
+
+      </div>
     )
   }
 }
